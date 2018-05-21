0863c0385e5ff7f2b516b7cfc1d9b9e8b09f526a
dilvan <dilvan@gmail.com>
2018-05-21 16:34:10 -0300

    Versão compartilhada com os professores.

--- src/main/groovy/corrector1.groovy
+++ src/main/groovy/corrector1.groovy
@@ -31,20 +31,28 @@
 import geb.Browser
 import geb.navigator.Navigator
 import groovy.io.FileType
+import groovy.transform.Field
 
 import java.time.LocalDateTime
 
-def parents(Navigator node){
-    def a = node
-    while (a) {
-        println a.tag() + ' - ' +a.classes()
-        a = a.parent()
+//def parents(Navigator node){
+//    def a = node
+//    while (a) {
+//        println a.tag() + ' - ' +a.classes()
+//        a = a.parent()
+//
+//    }
+//}
 
-    }
-}
+@Field
+Map criteria = [
+    onlyJournals: 2,
+    journals: 1,
+    conferences: 3
+]
 
 def findYear(text){
-    for (x in 2018..1980) {
+    for (x in 2018..1970) {
         if (text.contains(' '+x+'.') || text.contains(' '+x+','))
             return x
     }
@@ -58,99 +66,89 @@ def isICMC(String text){
     text.contains('ICMC')
 }
 
-def minmax(... list){
-    def max = -200000000
+def min(... list){
     def min =  200000000
     list.each {
         if (it>max) max=it
         if (it<min) min=it
     }
-    return [min, max]
+    return min
+}
+
+def getYear(Navigator node){
+    try {
+        int year = Integer.parseInt(node.find('div b').text().substring(0, 4))
+        return year
+    }
+    catch (Exception e) {return -1}
+
 }
 
 /**
- * Finds the assistant professor period beginning and ending dates.
+ * Finds when a professor entered USP.
  *
  * @param node
  * @return [beginning date, ending date]
  */
-def period(node){
-    int begin = 0
-    int end = 2019
+def start(Navigator node){
+    int begin = Integer.MAX_VALUE
     node.each{
         if (isICMC(it.find('b').text())){
-
-            int year = Integer.parseInt(it.next().next().next().find('div b').text().substring(0,4))
-            if (begin==0) begin = year
-            else {
-                end = Math.max(begin, year)
+            Navigator v = it.next().next().next()
+            int year = getYear(v)
+            if (year!=-1)
                 begin = Math.min(begin, year)
+            for (int i=0; i<2; i++) {
+                v = v.next().next().next().next().next()
+                year = getYear(v)
+                if (year!=-1)
+                    begin = Math.min(begin, year)
             }
-            //println it.next().next().next().next().next().next().find('div b').text()
-            try {
-                if (it.next().next().next().next().next().next().find('div b').text().contains('Vínculo institucional')) {
-                    int year2 = Integer.parseInt(it.next().next().next().next().next().next().next().next().find('div b').text().substring(0, 4))
-                    if (year != year2) {
-                        end = Math.max(begin, year2)
-                        begin = Math.min(begin, year2)
-                    }
-                    try {
-                        //println it.next().next().next().next().next().next().next().next().next().next().next().tag()
-
-                        if (it.next().next().next().next().next().next().next().next().next().next().next().find('div b').text().contains('Vínculo institucional')) {
-                            int year3 = Integer.parseInt(it.next().next().next().next().next().next().next().next().next().next().next().next().next().find('div b').text().substring(0, 4))
-                            if (year3 != year && year3 != year2)
-                                (begin, end) = minmax(year, year2, year3)
-                        }
-                    }
-                    catch (Exception e) {}
-                }
-            }
-            catch (Exception e) {}
         }
     }
-    [begin, end]
+    begin
 }
 
 /**
- * Count number of years in lst inside the closed interval [year year+4]
+ * Count number of years in lst inside the closed interval [year-4 year]
  *
  * @param lst - List of years
- * @param year - first year: [year year+4]
+ * @param year - last year: [year-4 year]
  * @return number or years in the interval
  */
 //
 def num(List lst, int year){
     int count = 0
-    lst.each{if (it >= year && it < (year+5)) ++count}
+    lst.each{if (it >= (year-4) && it <= year) ++count}
     count
 }
 
 /**
- * Finds the percentage of years that an assistant professor fulfills the position criteria.
+ * Finds the percentage of years that a professor fulfills the position criteria.
  *
  * @param prof - map with professor data
  * @param report - report file
  * @return
  */
-def analysis(Map prof, report){
-    int art = 2
-    int confArt = 1
-    int confConf = 3
+def analysis(Map prof, a, b, report){
+    int art = criteria.onlyJournals
+    int confArt = criteria.journals
+    int confConf = criteria.conferences
     println "Criterio: $art em periodicos OU $confArt em periodicos e $confConf em conferencias."
     report << "Criterio: $art em periodicos OU $confArt em periodicos e $confConf em conferencias.\n"
 
     int ok = 0
-    for (x in prof.begin..(prof.end-5)){
-        def articles = num(prof.articles, x)
+    for (x in a..b){
+        //println("x $x")
+        def journals = num(prof.articles, x)
         def confs = num(prof.confs, x)
-        if (articles>=art || (articles>=confArt && confs>=confConf)) ++ok
+        if (journals>=art || (journals>=confArt && confs>=confConf)) ++ok
         else {
-            println "Nao atendeu em $x - ${x+4} [journals: $articles conf: $confs]."
-            report << "Nao atendeu em $x - ${x+4} [journals: $articles conf: $confs].\n"
+            println "Nao atendeu em ${x-4} - $x [journals: $journals conf: $confs]."
+            report << "Nao atendeu em ${x-4} - $x [journals: $journals conf: $confs].\n"
         }
     }
-    (ok*1.0)/(prof.end - prof.begin - 4)
+    (ok*1.0)/(b-a+1)
 }
 
 Browser.drive {
@@ -165,9 +163,6 @@ Browser.drive {
         println( file.name)
     }
 
-    //println 'file:///home/dilvan/Dropbox/IdeaProjects/webpageCorrector/src/main/resources/'+list[0]
-    //println 'file:///home/dilvan/Dropbox/IdeaProjects/webpageCorrector/src/main/resources/Curr%C3%ADculo%20do%20Sistema%20de%20Curr%C3%ADculos%20Lattes%20(Dilvan%20de%20Abreu%20Moreira).html'
-
     def report = new File('report.txt')
     if (report.exists())
         report.delete()
@@ -175,34 +170,36 @@ Browser.drive {
     report << LocalDateTime.now() as String
     report << '\n\n'
 
-    //println $('h2.nome', 0).text()
     for (filename in list) {
 
         println(URLDecoder.decode(filename))
         //go "file:///home/dilvan/Dropbox/IdeaProjects/webpageCorrector/src/main/resources/Curr%C3%ADculo%20do%20Sistema%20de%20Curr%C3%ADculos%20Lattes%20(Jos%C3%A9%20Fernando%20Rodrigues%20J%C3%BAnior).html"
         //go 'file:///home/dilvan/Dropbox/IdeaProjects/webpageCorrector/src/main/resources/Curr%C3%ADculo%20do%20Sistema%20de%20Curr%C3%ADculos%20Lattes%20(Dilvan%20de%20Abreu%20Moreira).html'
-        //assert title == "Geb - Very Groovy Browser Automation"
         //go 'file:///home/dilvan/Dropbox/IdeaProjects/webpageCorrector/src/main/resources/Curr%C3%ADculo%20do%20Sistema%20de%20Curr%C3%ADculos%20Lattes%20(Jo%C3%A3o%20Lu%C3%ADs%20Garcia%20Rosa).html'
+        //go 'file:///home/dilvan/Dropbox/IdeaProjects/webpageCorrector/src/main/resources/Curr%C3%ADculo%20do%20Sistema%20de%20Curr%C3%ADculos%20Lattes%20(Rosane%20Minghim).html'
+        //go 'file:///home/dilvan/Dropbox/IdeaProjects/webpageCorrector/src/main/resources/Curr%C3%ADculo%20do%20Sistema%20de%20Curr%C3%ADculos%20Lattes%20(Rudinei%20Goularte).html'
 
         go 'file:///home/dilvan/Dropbox/IdeaProjects/webpageCorrector/src/main/resources/'+filename
 
         def prof = [:]
         prof.name = $('h2.nome').text().split('\n')[0]
-        def (begin, end) = period($('div.inst_back'))
-        prof.begin = begin
-        prof.end = end
+        prof.begin = start($('div.inst_back'))
         prof.articles = []
         prof.confs = []
 
+        // Year that became an associate
+        def ld = $('a[name$="LivreDocencia"]')
+        if (!ld.empty)
+            prof.associate = Integer.parseInt( ld.next().next().find('b', 0).text())
+        else
+            prof.associate = 2018
+
         report << prof.name + '\n'
         report << "Inicio Doutor: $prof.begin\n"
-        report << "Fim Doutor: $prof.end\n"
+        report << "Fim Doutor: $prof.associate\n"
 
-        $("#artigos-completos").find('div.artigo-completo').each {
+        $('#artigos-completos div.artigo-completo').each {
             if (it.find('sup img[src$="jcr.gif"]').empty) return
-            //def art = [:]
-            //art.year = it.find('span[data-tipo-ordenacao="ano"]').text()
-            // art.jcr = !it.find('sup img[src$="jcr.gif"]').empty
             prof.articles << Integer.parseInt(it.find('span[data-tipo-ordenacao="ano"]').text())
         }
 
@@ -228,20 +225,12 @@ Browser.drive {
         report << "Conferencias: $prof.confs\n"
 
         println(prof)
+        //float per = 100 * analysis(prof.begin, prof.associate-5, 2017, report)
+        float per = 100 * analysis(prof, prof.begin+4, prof.associate-1, report)
 
-        float per = 100 * analysis(prof, report)
+        //float per = 100 * analysis(prof, prof.associate, 2017, report)
         per = per.round(1)
         println "Percentagem do tempo que atende aos requisitos $per%.\n"
         report << "Percentagem do tempo que atende aos requisitos $per%.\n\n"
     }
-//    println $("#artigos-completos").find("div.artigo-completo").find('span',3).text() //.getAttribute("data-tipo-ordenacao")
-//    parents($("#artigos-completos").find("div.artigo-completo").find('span',3))
-//    println $("div.combo h1", 0).text()
-//    println $("span.informacao-artigo")
-//    waitFor { !$("#manuals-m").hasClass("animating") }
-//
-//    $("#manuals-menu a")[0].click()
-//
-
-//    assert title.startsWith("The Book Of Geb")
 }
